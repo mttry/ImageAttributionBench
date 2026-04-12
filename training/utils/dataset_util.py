c26313b713579f53fc17e951755d4c4d9a8ce404
mttry <2321763063@qq.com>
2026-04-12 07:56:37 +0000

    chore: move FLUX2_KLEIN and Z_IMAGE to data/

--- training/utils/dataset_util.py
+++
@@ -1,9 +0,0 @@
-class ConfigToAttr:  
-    def __init__(self, config):  
-        for k, v in config.items():  
-            setattr(self, k, v)  
-        # 也保留原始字典  
-        self.opt = config 
-
-    def get(self,key,default):
-        return self.opt.get(key,default)
